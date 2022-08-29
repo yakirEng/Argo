5e043ebed495068cdc6f42f1505b23d911f0784a
yakirEng <yakirhadad18@gmail.com>
2022-08-29 12:54:42 +0300

    replaced file

--- data_generation.py
+++ data_generation.py
@@ -3,14 +3,17 @@ import os
 import random
 import numpy as np
 from numpy.linalg import inv
+import time
 
-train_path = '/data/train2017'
+
+train_path = '/Data/train'
 validation_path = '/data/val2017'
 test_path = '/data/test2017'
 
 
-def ImagePreProcessing(image, path):
-    img = cv2.imread(path + '/%s' % image, 0)
+
+def ImagePreProcessing(image_name, path):
+    img = cv2.imread(path + '/%s' % image_name, 0)
     img = cv2.resize(img, (320, 240))
 
     rho = 32
@@ -31,7 +34,7 @@ def ImagePreProcessing(image, path):
 
     warped_image = cv2.warpPerspective(img, H_inverse, (320, 240))
 
-    annotated_warp_image = warped_image.copy()
+    # annotated_warp_image = warped_image.copy()
 
     Ip1 = test_image[top_point[1]:bottom_point[1], top_point[0]:bottom_point[0]]
     Ip2 = warped_image[top_point[1]:bottom_point[1], top_point[0]:bottom_point[0]]
@@ -39,7 +42,6 @@ def ImagePreProcessing(image, path):
     training_image = np.dstack((Ip1, Ip2))
     H_four_points = np.subtract(np.array(perturbed_four_points), np.array(four_points))
     datum = (training_image, H_four_points)
-
     return datum
 
 
@@ -52,6 +54,28 @@ def savedata(path):
         np.save(new_path + '%s' % i[0:12], ImagePreProcessing(i, path))
 
 
-savedata(train_path)
-savedata(validation_path)
-savedata(test_path)
+def pre_processing(source_paths, target_paths):
+    start_time = time.time()
+    for path_idx, src_path in enumerate(source_paths):
+        images_names = os.listdir(src_path)
+        print(f'start processing images from {src_path} to {target_paths[path_idx]}')
+        print(f'time stamp: {time.time() - start_time:.2f} seconds')
+        for image_idx, image_name in enumerate(images_names):
+            datum = ImagePreProcessing(image_name, src_path)
+            np.save(target_paths[path_idx] + '/' + f'{image_idx}.npy', datum)
+            print(f'image {image_idx} processed')
+
+
+
+
+
+
+
+
+# savedata(train_path)
+# savedata(validation_path)
+# savedata(test_path)
+
+source_paths = [r'D:\Academic\Msc\Thesis\AgroCode\Projection\Data\train', r'D:\Academic\Msc\Thesis\AgroCode\Projection\Data\test']
+target_paths = [r"D:\Academic\Msc\Thesis\AgroCode\Projection\Data\processed\train", r'D:\Academic\Msc\Thesis\AgroCode\Projection\Data\processed\test']
+pre_processing(source_paths, target_paths)
