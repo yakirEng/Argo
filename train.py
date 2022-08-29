31bc0b075f8ebfbba86c139c783af2a08a43a73f
yakirEng <yakirhadad18@gmail.com>
2022-08-29 11:43:10 +0300

    add "plot_loss" and "test" functions

--- train.py
+++ train.py
@@ -51,7 +51,6 @@ def train():
     batch_size = 64
     TrainLoader = DataLoader(TrainingData, batch_size)
     ValidationLoader = DataLoader(ValidationData, batch_size)
-    TestLoader = DataLoader(TestData, batch_size)
     criterion = nn.MSELoss()
     num_samples = 118287
     total_iteration = 90000
@@ -60,6 +59,8 @@ def train():
     model = RegressionModel().to(device)
     summary(model, (2, 128, 128))
     optimizer = optim.SGD(model.parameters(), lr=0.005, momentum=0.9)
+    train_loss, val_loss, train_losses, val_losses = [], [], [], []
+    min_loss = 9999
     for epoch in range(epochs):
         for i, (images, target) in enumerate(TrainLoader):
             optimizer.zero_grad()
@@ -71,25 +72,65 @@ def train():
             loss = criterion(outputs, target.view(-1, 8))
             loss.backward()
             optimizer.step()
-            if (i + 1) % len(TrainLoader) == 0:
-                print('Train Epoch: [{}/{}] [{}/{} ({:.0f}%)]\Mean Squared Error: {:.6f}'.format(
-                    epoch + 1, epochs, i, len(TrainLoader),
-                    100. * i / len(TrainLoader), loss))
-                plot_loss()
+            train_loss_vec.append(loss)
 
-    state = {'epoch': epochs, 'state_dict': model.state_dict(),
-             'optimizer': optimizer.state_dict()}
-    torch.save(state, 'DeepHomographyEstimation.pth')
+        model.eval()
+        with torch.no_grad():
+            for i, (images, target) in enumerate(ValidationLoader):
+                images = images.to(device)
+                target = target.to(device)
+                images = images.permute(0, 3, 1, 2).float()
+                target = target.float()
+                outputs = model(images)
+                loss = criterion(outputs, target.view(-1, 8))
+                val_loss_vec.append(loss)
 
-    # Evaluate on validation set
+        train_loss = np.mean(train_loss_vec)
+        val_loss = np.mean(val_loss_vec)
 
+        print('Train Epoch: [{}/{}] [{}/{} ({:.0f}%)]\,MSE train loss: {:.6f}, MSE val loss: {:.6f}'.format(
+            epoch + 1, epochs, i, len(TrainLoader),
+            100. * i / len(TrainLoader), train_loss, val_loss))
+
+        train_losses.append(train_loss)
+        val_losses.append(val_loss)
+
+        plot_loss(epochs, train_losses, val_losses)
+
+        if np.mean(val_loss) < min_loss:
+            state = {'epoch': epochs, 'state_dict': model.state_dict(),
+                    'optimizer': optimizer.state_dict()}
+            torch.save(state, 'DeepHomographyEstimation.pth')
+            min_loss = np.mean(val_loss)
+
+        train_loss_vec = []
+        val_loss_vec = []
+
+def test():
+    batch_size = 64
+    TestLoader = DataLoader(TestData, batch_size)
+    model = RegressionModel().to(device)
+    criterion = nn.MSELoss()
     model.eval()
+    tst_loss_vec = []
     with torch.no_grad():
-        for i, (images, target) in enumerate(ValidationLoader):
+        for i, (images, target) in enumerate(TestLoader):
             images = images.to(device)
             target = target.to(device)
             images = images.permute(0, 3, 1, 2).float()
             target = target.float()
             outputs = model(images)
             loss = criterion(outputs, target.view(-1, 8))
-            print('\Mean Squared Error: {:.6f}'.format(loss))
+            tst_loss_vec.append(loss)
+        print('test loss: ' + np.mean(tst_loss_vec))
+
+
+
+def plot_loss(epochs, train_loss, eval_loss):
+    plt.plot(range(epochs), train_loss)
+    plt.plot(range(epochs), eval_loss)
+    plt.grid()
+    plt.title('MSE loss')
+    plt.xlabel('epochs')
+    plt.ylabel('loss')
+    plt.legend('train', 'validation')
